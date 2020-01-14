a1c78002e7f9618f9752f2794a0c15a0a534e075
Ugnius <ugniusnaujokas@gmail.com>
2020-01-14 10:54:58 +0200

    Completed App1

---
+++ App1/src/lt/codeacademy/Main.java
@@ -0,0 +1,13 @@
+package lt.codeacademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 7;
+        int b = 7;
+        double rez;
+        rez = (double)(a*b) / 5;
+        System.out.println(rez);
+    }
+}
